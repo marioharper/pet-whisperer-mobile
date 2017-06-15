4f3c89387d927756e43ba524656a9fa22eca7f2d
marioharper <harpmari12@gmail.com>
2017-06-15 17:03:53 -0500

    checkin

--- app/containers/createPetActivity/components/screen/index.js
+++ app/containers/createPetActivity/components/screen/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import CSS from './styles';
 import Form from '../form';
 
-const Screen = ({ navigation }) => {
+const Screen = ({ navigation, createPetActivity }) => {
   const { pet } = navigation.state.params;
 
   return (
     <View style={CSS.container}>
-      <Form pet={pet} onSubmit={data => console.log('form submitted', data)} />
+      <Form pet={pet} onSubmit={activity => createPetActivity(pet.id, activity)} />
     </View>
   );
 };
@@ -16,5 +17,16 @@ const Screen = ({ navigation }) => {
 Screen.navigationOptions = () => ({
   title: 'Add Activity',
 });
+
+Screen.propTypes = {
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        pet: PropTypes.object.isRequired,
+      }),
+    }),
+  }).isRequired,
+  createPetActivity: PropTypes.func.isRequired,
+};
 
 export default Screen;
